81151691674fe2c3c6628f7040b59ab3282a1fed
graerook156 <graerook156@SDS-L-49QLG63>
2025-01-23 10:18:09 -0500

    Add project files.

---
+++ not hatsune miku/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace not_hatsune_miku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.tickLabel = new System.Windows.Forms.Label();
+            this.lifeLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.skillLabel = new System.Windows.Forms.Label();
+            this.comboLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // tickLabel
+            // 
+            this.tickLabel.AutoSize = true;
+            this.tickLabel.BackColor = System.Drawing.Color.Transparent;
+            this.tickLabel.ForeColor = System.Drawing.Color.Black;
+            this.tickLabel.Location = new System.Drawing.Point(13, 13);
+            this.tickLabel.Name = "tickLabel";
+            this.tickLabel.Size = new System.Drawing.Size(0, 16);
+            this.tickLabel.TabIndex = 0;
+            // 
+            // lifeLabel
+            // 
+            this.lifeLabel.AutoSize = true;
+            this.lifeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.lifeLabel.Location = new System.Drawing.Point(444, 13);
+            this.lifeLabel.Name = "lifeLabel";
+            this.lifeLabel.Size = new System.Drawing.Size(44, 16);
+            this.lifeLabel.TabIndex = 1;
+            this.lifeLabel.Text = "label1";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Location = new System.Drawing.Point(225, 13);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(44, 16);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "label1";
+            // 
+            // skillLabel
+            // 
+            this.skillLabel.BackColor = System.Drawing.Color.Transparent;
+            this.skillLabel.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.skillLabel.Location = new System.Drawing.Point(180, 293);
+            this.skillLabel.Name = "skillLabel";
+            this.skillLabel.Size = new System.Drawing.Size(136, 85);
+            this.skillLabel.TabIndex = 3;
+            this.skillLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboLabel
+            // 
+            this.comboLabel.AutoSize = true;
+            this.comboLabel.BackColor = System.Drawing.Color.Transparent;
+            this.comboLabel.Location = new System.Drawing.Point(239, 362);
+            this.comboLabel.Name = "comboLabel";
+            this.comboLabel.Size = new System.Drawing.Size(44, 16);
+            this.comboLabel.TabIndex = 4;
+            this.comboLabel.Text = "label1";
+            this.comboLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 800);
+            this.Controls.Add(this.comboLabel);
+            this.Controls.Add(this.skillLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.lifeLabel);
+            this.Controls.Add(this.tickLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label tickLabel;
+        private System.Windows.Forms.Label lifeLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label skillLabel;
+        private System.Windows.Forms.Label comboLabel;
+    }
+}
+
---
+++ not hatsune miku/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace not_hatsune_miku
+{
+    public partial class Form1 : Form
+    {
+        //define all the variables
+        bool dPressed = false;
+        bool fPressed = false;
+        bool jPressed = false;
+        bool kPressed = false;
+
+        bool escPressed = false;
+
+
+        int tick = 0;
+        int score = 0;
+        int life = 100;
+        int combo = 0;
+
+
+        //create the note list
+        List<Rectangle> notes = new List<Rectangle>();
+
+        //visible hitzones
+        Rectangle d = new Rectangle(0, 620, 50, 30);
+        Rectangle f = new Rectangle(100, 620, 50, 30);
+        Rectangle j = new Rectangle(200, 620, 50, 30);
+        Rectangle k = new Rectangle(300, 620, 50, 30);
+
+
+        //define actual hitzones
+        Rectangle DPe = new Rectangle(0, 610, 50, 40);
+        Rectangle DGr = new Rectangle(0, 580, 50, 30);
+        Rectangle DGo = new Rectangle(0, 550, 50, 30);
+
+
+        Rectangle FPe = new Rectangle(100, 610, 50, 40);
+        Rectangle FGr = new Rectangle(100, 580, 50, 30);
+        Rectangle FGo = new Rectangle(100, 550, 50, 30);
+
+
+        Rectangle JPe = new Rectangle(200, 610, 50, 40);
+        Rectangle JGr = new Rectangle(200, 580, 50, 30);
+        Rectangle JGo = new Rectangle(200, 550, 50, 30);
+
+
+        Rectangle KPe = new Rectangle(300, 610, 50, 40);
+        Rectangle KGr = new Rectangle(300, 580, 50, 30);
+        Rectangle KGo = new Rectangle(300, 550, 50, 30);
+
+
+        Random random = new Random();
+
+
+        //define brushes used
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+
+        //define debugging brushes
+        SolidBrush peHitzone = new SolidBrush(Color.Red);
+        SolidBrush grHitzone = new SolidBrush(Color.Gray);
+        SolidBrush goHitzone = new SolidBrush(Color.Green);
+
+        //define note lanes
+        int laneD = 0;
+        int laneF = 100;
+        int laneJ = 200;
+        int laneK = 300;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //creating notes in random postions
+            if (tick % 20 == 0)
+            {
+                int laneChoice = random.Next(1, 5);
+                
+                if (laneChoice == 1)
+                {
+                    Rectangle newNote = new Rectangle(laneD, 0, 50, 30);
+                    notes.Add(newNote);
+                }
+                else if (laneChoice == 2)
+                {
+                    Rectangle newNote = new Rectangle(laneF, 0, 50, 30);
+                    notes.Add(newNote);
+                }
+                else if (laneChoice == 3)
+                {
+                    Rectangle newNote = new Rectangle(laneJ, 0, 50, 30);
+                    notes.Add(newNote);
+                }
+                else if (laneChoice == 4)
+                {
+                    Rectangle newNote = new Rectangle(laneK, 0, 50, 30);
+                    notes.Add(newNote);
+                }
+            }
+
+            //check for collision with the "D" hitzones
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (dPressed && DPe.IntersectsWith(notes[i]))
+                //if it collides with any hitzones, add score, change combo, update accuracy label, and remove note
+                {
+                    notes.RemoveAt(i);
+                    dPressed = false;
+                    score += 100;
+                    combo++;
+                    skillLabel.Text = "Perfect";
+                }
+                else if (dPressed && DGr.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    dPressed = false;
+                    score += 50;
+                    combo++;
+                    skillLabel.Text = "Great";
+                }
+                else if (dPressed && DGo.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    dPressed = false;
+                    score += 30;
+                    combo = 0;
+                    skillLabel.Text = "Good";
+                }
+                else if (dPressed && d.IntersectsWith(notes[i]) == false)
+                {
+                    //remove life if missed
+                    life -= 5;
+                    combo = 0;
+                    dPressed = false;
+                    skillLabel.Text = "Miss";
+                }
+            }
+
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (fPressed && FPe.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    fPressed = false;
+                    score += 100;
+                    combo++;
+                    skillLabel.Text = "Perfect";
+                }
+                else if (fPressed && FGr.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    fPressed = false;
+                    score += 50;
+                    combo++;
+                    skillLabel.Text = "Great";
+                }
+                else if (fPressed && FGo.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    fPressed = false;
+                    score += 30;
+                    combo = 0;
+                    skillLabel.Text = "Good";
+                }
+                else if (fPressed && f.IntersectsWith(notes[i]) == false)
+                {
+                    life -= 5;
+                    combo = 0;
+                    fPressed = false;
+                    skillLabel.Text = "Miss";
+                }
+            }
+
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (jPressed && JPe.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    jPressed = false;
+                    score += 100;
+                    combo++;
+                    skillLabel.Text = "Perfect";
+                }
+                else if (jPressed && JGr.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    jPressed = false;
+                    score += 50;
+                    combo++;
+                    skillLabel.Text = "Great";
+                }
+                else if (jPressed && JGo.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    jPressed = false;
+                    score += 30;
+                    combo = 0;
+                    skillLabel.Text = "Good";
+                }
+                else if (jPressed && j.IntersectsWith(notes[i]) == false)
+                {
+                    life -= 5;
+                    combo = 0;
+                    jPressed = false;
+                    skillLabel.Text = "Miss";
+                }
+            }
+
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (kPressed && KPe.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    kPressed = false;
+                    score += 100;
+                    combo++;
+                    skillLabel.Text = "Perfect";
+                }
+                else if (kPressed && KGr.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    kPressed = false;
+                    score += 50;
+                    combo++;
+                    skillLabel.Text = "Great";
+                }
+                else if (kPressed && KGo.IntersectsWith(notes[i]))
+                {
+                    notes.RemoveAt(i);
+                    kPressed = false;
+                    score += 30;
+                    combo = 0;
+                    skillLabel.Text = "Good";
+                }
+                else if (kPressed && k.IntersectsWith(notes[i]) == false)
+                {
+                    life -= 5;
+                    combo = 0;
+                    kPressed = false;
+                    skillLabel.Text = "Miss";
+                }
+            }
+
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (notes[i].Y > this.Height)
+                {
+                    notes.RemoveAt(i);
+                    life -= 5;
+                    combo = 0;
+                    skillLabel.Text = "Miss";
+                }
+            }
+
+            for (int i = 0; i < notes.Count; i++)
+            {
+                int y = notes[i].Y + 5;
+                notes[i] = new Rectangle(notes[i].X, y, 50, 30);
+            }
+
+            tick++;
+
+            if (escPressed == true)
+            {
+                Close();
+            }
+
+            if (life == 0)
+            {
+                gameTimer.Stop();
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //draw all relevant variables
+            tickLabel.Text = $"Tick: {tick}";
+            lifeLabel.Text = $"Life {life}";
+            scoreLabel.Text = $"Score {score}";
+            comboLabel.Text = $"{combo}";
+
+
+            //draw hitzones
+            e.Graphics.FillRectangle(blackBrush, d);
+            e.Graphics.FillRectangle(blackBrush, f);
+            e.Graphics.FillRectangle(blackBrush, j);
+            e.Graphics.FillRectangle(blackBrush, k);
+
+
+            //draw actual hitzones for debugging
+            //e.Graphics.FillRectangle(peHitzone, DPe);
+            //e.Graphics.FillRectangle(grHitzone, DGr);
+            //e.Graphics.FillRectangle(goHitzone, DGo);
+
+
+            //draw notes
+            for (int i = 0; i < notes.Count; i++)
+            {
+                e.Graphics.FillRectangle(blueBrush, notes[i]);
+            }
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //check for when buttons are pressed
+            switch (e.KeyChar)
+            {
+                case 'd':
+                    dPressed = true;
+                    break;
+                case 'f':
+                    fPressed = true;
+                    break;
+                case 'j':
+                    jPressed = true;
+                    break;
+                case 'k':
+                    kPressed = true;
+                    break;
+                case 'e':
+                    escPressed = true;
+                    break;
+            }
+        }
+    }
+}
